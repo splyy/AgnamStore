e11b498ebede252dcef3652e1d6f6d69ddf58b6a
agory <gory.alexandre02@gmail.com>
2014-12-16 14:51:11 +0100

    AgnamStore 0.2.1

---
+++ web/js/genre.js
@@ -0,0 +1,38 @@
+jQuery(document).ready(function(){
+    var $listGenre = $('#list-genre');
+	var $listGenreAdd = $('#list-genre-add');
+	var $buttonAdd= $('#add-genre');
+	var $buttonRemove= $('#remove-genre');
+	var $itemGenreId = $('#item-genre-id')
+	
+	$buttonAdd.click(function(){
+		var $idGenre = $listGenre.val();
+		var $optionSelected = $('#list-genre option[value='+$idGenre+']');
+		$listGenreAdd.append($optionSelected);
+		if($idGenre != null){
+			$itemGenreId.val($itemGenreId.val()+$idGenre+'-')
+		}
+		
+	})
+	
+	$buttonRemove.click(function(){
+		var $idGenre = $listGenreAdd.val();
+		var $optionSelected = $('#list-genre-add option[value='+$idGenre+']');
+		$listGenre.append($optionSelected);
+		var $idGenres = $itemGenreId.val();
+		var $nombre = "";
+		for(var i = 0; i < $idGenres.length ; i++){
+			if($idGenres[i] != '-'){
+				$nombre += $idGenres[i];
+			} else {
+				if($nombre == $idGenre){
+					alert($nombre);
+					$idGenres = $idGenres.replace($idGenres.substring(i - $nombre.length, i + 1),'');
+					alert($idGenres);
+					$itemGenreId.val($idGenres);
+				}
+				$nombre = "";
+			}
+		}
+	})
+});
