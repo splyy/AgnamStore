06126d700e07b040c99406c1e62455181d22c846
agory <gory.alexandre02@gmail.com>
2015-03-04 11:18:35 +0100

    AgnamStore 0.2.2 Avec correction

--- src/AgnamStore/Form/Type/User/UserProfilType.php
+++ src/AgnamStore/Form/Type/User/UserProfilType.php
@@ -9,12 +9,18 @@ class UserProfilType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-                ->add('email'       , 'email'   , array('required' => TRUE))
-                ->add('firstName'   , 'text'    , array('required' => TRUE))
-                ->add('lastName'    , 'text'    , array('required' => TRUE))
-                ->add('address'     , 'text'    , array('required' => TRUE))
-                ->add('city'        , 'text'    , array('required' => TRUE))
-                ->add('cp'          , 'number'  , array('required' => TRUE));
+                ->add('email', 'email', array('required' => TRUE))
+                ->add('firstName', 'text', array('required' => TRUE))
+                ->add('lastName', 'text', array('required' => TRUE))
+                ->add('address', 'text', array('required' => TRUE))
+                ->add('city', 'text', array('required' => TRUE))
+                ->add('cp', 'text', array(
+                    'required' => TRUE,
+                    'constraints' => array(new \Symfony\Component\Validator\Constraints\Regex(array(
+                            'pattern' => '#^[0-9]{5}$#',
+                            'match' => TRUE,
+                            'message' => 'Votre Code postal est incorrect'
+                        )))));
     }
 
     public function getName() {
